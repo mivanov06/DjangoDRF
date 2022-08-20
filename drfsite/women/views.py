e23961911dae121ba0dffd487bbcb9e10c9444f1
mivanov06 <mivanov06@yandex.ru>
2022-08-21 00:09:21 +1000

    Install DRF Create Serialiser

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
