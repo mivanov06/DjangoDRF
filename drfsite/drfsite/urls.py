c9799cabcc38afd1770308c246ff1f070cdf8e25
mivanov06 <mivanov06@yandex.ru>
2022-09-07 03:56:18 +1000

    #9. Роутеры: SimpleRouter и DefaultRouter

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -19,8 +19,31 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
+
+class MyCustomRouter(routers.SimpleRouter):
+
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        )
+    ]
+
+
+#router = MyCustomRouter()
 router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Women.objects.all()[:3]
+
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 
 
