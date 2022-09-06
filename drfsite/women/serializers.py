04631cf6ed078b3033b337fb63d1fa9365c4d522
mivanov06 <mivanov06@yandex.ru>
2022-09-07 04:55:06 +1000

    #10. Ограничения доступа (permissions)

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -4,6 +4,8 @@ from .models import Women
 
 
 class WomenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Women
         fields = "__all__"
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,45 +1,47 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Women, Category
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
-class WomenViewSet(viewsets.ModelViewSet):
-    # queryset = Women.objects.all()
+#
+
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
     serializer_class = WomenSerializer
-
-    def get_queryset(self):
-        pk = self.kwargs.get("pk")
-        if not pk:
-            return Women.objects.all()[:3]
-
-        return Women.objects.filter(pk=pk)
-
-    @action(methods=['get'], detail=True)
-    def category(self, request, pk=None):
-        cats = Category.objects.get(pk=pk)
-        return Response({'cats': cats.name})
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()  # возвращает одно значение
+    serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
-# возвращает список записей по GET запросу
-# добавляет новую запись по POST запросу
-# class WomenAPIList(generics.ListCreateAPIView):
-#     queryset = Women.objects.all()
+class WomenAPIDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()  # возвращает одно значение
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+
+# class WomenViewSet(viewsets.ModelViewSet):
+#     # queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
 #
+#     def get_queryset(self):
+#         pk = self.kwargs.get("pk")
+#         if not pk:
+#             return Women.objects.all()[:3]
 #
-# # изменение по PUT или PATCH запросу
-# class WomenAPIUpdate(generics.UpdateAPIView):
-#     queryset = Women.objects.all()      #возвращает одно значение
-#     serializer_class = WomenSerializer
+#         return Women.objects.filter(pk=pk)
 #
-#
-# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Women.objects.all()      #возвращает одно значение
-#     serializer_class = WomenSerializer
+#     @action(methods=['get'], detail=True)
+#     def category(self, request, pk=None):
+#         cats = Category.objects.get(pk=pk)
+#         return Response({'cats': cats.name})
