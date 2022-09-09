0ae396f4bdb0429e66ce7b379899d2b1b7fb8d71
mivanov06 <mivanov06@yandex.ru>
2022-09-09 17:27:03 +1000

    #11. Авторизация и аутентификация. Session-based authentication

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -27,6 +27,8 @@ from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
